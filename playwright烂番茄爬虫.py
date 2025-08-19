b51b38169887624f3d628ca8a69dca9a13a46a6d
StarrySkyVictor <1541698710@qq.com>
2025-08-19 15:05:47 +0800

    playwright烂番茄爬虫
    playwright烂番茄爬虫，以哪吒2为例

---
+++ playwright烂番茄爬虫.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(
+        "https://www.rottentomatoes.com/m/ne_zha_ii/reviews?type=user",
+        wait_until="domcontentloaded"
+    )
+    for _ in range(5):  # 共爬取五页内容
+        load_more = page.get_by_text("Load More")
+        if load_more.is_visible():
+            load_more.click()
+            page.wait_for_timeout(2000)
+        else:
+            break
+
+    reviews = page.locator("p.audience-reviews__review.js-review-text[data-qa='review-text']")
+    count = reviews.count()
+
+    for i in range(count):
+        text = reviews.nth(i).inner_text().strip()
+        print(f"{i+1}. {text}")
+
+    print("评论总数：", count)
+    browser.close() 
+
+
+
